7c5e3f0b75f1e99866de77f5a35835f5043af725
Amangupta <amangt9526@gmail.com>
2024-12-30 18:31:27 +0530

    adfg

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -7,31 +7,30 @@ const AboutPage = () => {
     return (
         <section className="py-10 sm:py-16 lg:py-20">
             <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-                <div className="h-[10rem] w-full -mt-24 flex flex-col items-center justify-center overflow-hidden rounded-md">
-                    <h2 className="text-5xl pb-5 font-poppins font-medium leading-tight tracking-wide text-cyan-400 hover:text-cyan-400 sm:text-5xl lg:text-5xl">
-                       <span className="text-white text-5xl font-poppins font-medium"> About</span>Us
-                    </h2>
-                    <div className="w-[40rem] h-10 relative">
-                        {/* Gradients */}
-                        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-                        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-                        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-                        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+        <div className="h-[10rem] w-full -mt-24g flex flex-col items-center justify-center overflow-hidden rounded-md">
+            <h2 className="text-3xl  font-poppins pb-5 font-medium leading-tight tracking-wide text-cyan-400 hover:text-cyan-400 sm:text-3xl lg:text-5xl">
+              <span className="text-white">About</span> Us
+            </h2>
+            <div className="w-[40rem] h-10 relative">
+              {/* Gradients */}
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
-                        {/* Core component */}
-                        <SparklesCore
-                            background="transparent"
-                            minSize={0.4}
-                            maxSize={1}
-                            particleDensity={1200}
-                            className="w-full h-full"
-                            particleColor="#030F26"
-                        />
-                        {/* Radial Gradient to prevent sharp edges */}
-                        <div className="absolute inset-0 w-full h-full  [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-                    </div>
-                </div>
-
+              {/* Core component */}
+              <SparklesCore
+                background="transparent"
+                minSize={0.4}
+                maxSize={1}
+                particleDensity={1200}
+                className="w-full h-full"
+                particleColor="#030F26"
+              />
+              {/* Radial Gradient to prevent sharp edges */}
+              <div className="absolute inset-0 w-full h-full  [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+            </div>
+        </div>
                 <div className="max-w-3xl mx-auto text-center">
                     <p className="text-white font-poppins max-w-2xl mx-auto -mt-6 text-base sm:text-lg lg:text-xl leading-relaxed text-secondary">
                         Nuqi boasts top-tier financial experts and elite technology talent, striving to execute proven investment tactics and craft a bespoke investment journey for you. Our skilled team, with decades of experience from esteemed institutions like Citibank, Standard Chartered, Credit Suisse, Dubai Islamic Bank, Microsoft, IBM, ICICI Bank, and EY, is committed to your financial growth.
