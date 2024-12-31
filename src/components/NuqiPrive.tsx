c6b61a9818e03b5d54441147300b691a2aa8d376
Amangupta <amangt9526@gmail.com>
2024-12-31 16:14:58 +0530

    mmmm

--- src/components/NuqiPrive.tsx
+++ src/components/NuqiPrive.tsx
@@ -14,38 +14,3 @@ export const NuqiPrive: React.FC = () => {
     />
   );
 };
-// import * as React from "react";
-// import { HeroSection } from "./HeroSection";
-
-// export const NuqiPrive: React.FC = () => {
-//   return (
-//     <HeroSection
-//       title="Prive"
-//       highlightedText="Nuqi"
-//       imageSrc="/priveeeeee 4.png"
-//       description={`
-//         Welcome to NUQI Prive: Tailored Solutions 
-//         for Ultra-High Net Worth Individuals. 
-        
-//         At NUQI Prive, we understand that managing wealth is more than just numbers—it's about preserving and growing legacies while addressing unique financial aspirations. As our dedicated asset management division, NUQI Prive caters exclusively to Ultra-High Net Worth Individuals (UHN) with bespoke services that reflect our commitment to excellence and discretion.
-//       `}
-//     />
-//   );
-// };
-
-
-
-
-
-// export const NuqiPrive: React.FC = () => {
-//   return (
-//     <HeroSection
-//       title="Prive"
-//       highlightedText="Nuqi"
-//       imageSrc="/priveeeeee 4.png"
-//       description="Welcome to NUOI Prive: Tailored Solutions for Ultra-High Net Worth Individual
-
-// At NUQI Prive, we understand that managing wealth is more than just numbers-it's about preserving and growing legacies while addressing unique financial aspirations. As our dedicated asset management division, NUQI Prive caters exclusively to Ultra-High Net Worth Individuals (UHN) with bespoke services that reflect our commitment to excellence and discretion."
-//     />
-//   );
-// };
